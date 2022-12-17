121c25c7c5edc55260808508cee9e00b744272a7
User_E <gogo@tu.net>
2022-12-17 03:22:03 +0300

    Добавлено упражнение 4.4.2

---
+++ Ex 4.4.1.py
@@ -0,0 +1,5 @@
+# напишите сюда условие проверки нечётности чисел А и B
+number_a = int(input("Введите число A:"))
+number_b = int(input("Введите число B:"))
+if number_a % 2 and number_b % 2:
+    print('Числа А и B нечетные')
