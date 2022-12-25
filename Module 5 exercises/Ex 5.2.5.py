234b2ec3e5d7f36ffa3929e45c9406912a4a4c43
User_E <gogo@tu.net>
2022-12-26 02:51:39 +0300

    Добавлено упражнение 5.2.5

--- Module 5 exercises/Ex 5.2.5.py
+++ Module 5 exercises/Ex 5.2.5.py
@@ -9,3 +9,18 @@ def dev_a(a):
 
 a = int(input("Введите число: "))
 print("Число делителей: ", dev_a(a))
+
+"""
+Или
+
+def get_multipliers(a):
+   count = 1
+   for n in range(1, a // 2 + 1):
+       if a % n == 0:
+           count += 1
+
+   return count
+
+get_multipliers(5)  # 2
+get_multipliers(4)  # 3
+"""
