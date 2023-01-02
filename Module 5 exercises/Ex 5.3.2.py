cb06ab38f206012d56f940ff3e0ca23d8992c062
User_E <gogo@tu.net>
2023-01-02 17:55:55 +0300

    Добавлено упражнение 5.3.2

---
+++ Module 5 exercises/Ex 5.3.2.py
@@ -0,0 +1,12 @@
+#Написать функцию, которая будет перемножать любое количество переданных ей аргументов.
+def adder(*nums):
+    sum_ = 1
+    for n in nums:
+       #print(n)
+       #print("nums: ", nums)
+       sum_ *= n
+
+    return sum_
+
+
+print(adder(1, 2, 7))  # 6
