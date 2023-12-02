819dde1f086884a32f71ddb283fae19856d602db
AlekseiEvdokimov <54586112+AlekseiEvdokimov@users.noreply.github.com>
2023-12-02 22:25:42 +0300

    Задание 6.4.3 - окончательное решение квадратных уравнений

--- Module 6 exercises/Ex 6.4.3.py
+++ Module 6 exercises/Ex 6.4.3.py
@@ -5,6 +5,18 @@ def discriminant(a, b, c):
     return D
 
 
-print(discriminant(2, 2, 2))
-print(discriminant(1, -4, 4))
-print(discriminant(1, 3, -4))
+def quadratic_solve(a, b, c):
+    if discriminant(a, b, c) < 0:
+        return "Нет вещественных корней"
+    elif discriminant(a, b, c) > 0:
+        x1 = (-b - discriminant(a, b, c)**0.5)/(2*a)
+        x2 = (-b + discriminant(a, b, c)**0.5)/(2*a)
+        return x1, x2
+    else:
+        x = -b / (2 * a)
+        return x
+
+
+print(quadratic_solve(2, 2, 2))
+print(quadratic_solve(1, -4, 4))
+print(quadratic_solve(1, 3, -4))
