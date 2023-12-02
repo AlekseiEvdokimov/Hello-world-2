91ca4a237b7405ccd5d316fe5bfa824f52507e99
AlekseiEvdokimov <54586112+AlekseiEvdokimov@users.noreply.github.com>
2023-12-02 22:39:44 +0300

    Задание 6.4.3 - окончательное решение квадратных уравнений

--- Module 6 exercises/Ex 6.4.3.py
+++ Module 6 exercises/Ex 6.4.3.py
@@ -21,3 +21,7 @@ def quadratic_solve(a, b, c):
 print(quadratic_solve(2, 2, 2))
 print(quadratic_solve(1, -4, 4))
 print(quadratic_solve(1, 3, -4))
+
+print(type(quadratic_solve(2, 2, 2)))
+print(type(quadratic_solve(1, -4, 4)))
+print(type(quadratic_solve(1, 3, -4)))
